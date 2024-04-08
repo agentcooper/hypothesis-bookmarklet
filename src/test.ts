9202fb47303f654b0f933999c0900951ef91dabf
Artem Tyurin <artem.tyurin@gmail.com>
2024-04-08 14:02:15 +0200

    Update test page

--- src/test.ts
+++ src/test.ts
@@ -6,20 +6,21 @@ const annotationManager = new AnnotationManager({
   },
 });
 
+const testAnnotations = [
+  {
+    id: "a",
+    exact: `Before the reintroduction of separated text (spaces between words) in the late Middle Ages, the ability to read silently was considered rather remarkable.`,
+  },
+  { id: "b", exact: "have more education" },
+];
+annotationManager.setAnnotations(testAnnotations);
+
 document.getElementById("button-highlight")!.addEventListener("click", () => {
   const annotation = AnnotationManager.createAnnotationFromSelection();
   if (!annotation) {
     return;
   }
   annotationManager.addAnnotation(annotation);
-});
-
-document.getElementById("button-set")!.addEventListener("click", () => {
-  const testAnnotations = [
-    { id: "a", exact: "bachelor cheerful of mistaken" },
-    { id: "b", exact: "missed lovers" },
-  ];
-  annotationManager.setAnnotations(testAnnotations);
 });
 
 document.getElementById("button-remove-all")!.addEventListener("click", () => {
